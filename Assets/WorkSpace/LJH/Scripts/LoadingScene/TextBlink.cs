55003d72f1fcc2ff9a4267731540a82d1ef1d328
LeeJaeHo-96 <towpam123@gmail.com>
2025-04-30 18:33:49 +0900

    [Create][Feat] 로딩씬 생성 및 구현

---
+++ Assets/WorkSpace/LJH/Scripts/LoadingScene/TextBlink.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TextBlink : MonoBehaviour
+{
+    private Color curColor;
+    private Color originColor;
+
+
+    void Start()
+    {
+        curColor = gameObject.GetComponent<TextMeshProUGUI>().color;
+
+        StartCoroutine(BlinkCoroutine());
+    }
+
+
+    IEnumerator BlinkCoroutine()
+    {
+        bool isDecreasing = true;
+
+        while (true)
+        {
+            Debug.Log("코루틴 돌아가는중");
+            yield return new WaitForSeconds(0.01f);
+
+            if (isDecreasing)
+            {
+                MinusAlpha();
+                if (curColor.a <= 0.25f)
+                {
+                    isDecreasing = false;
+                }
+            }
+            else
+            {
+                PlusAlpha();
+                if (curColor.a >= 0.99f)
+                {
+                    isDecreasing = true;
+                }
+            }
+        }
+    }
+
+
+    void MinusAlpha()
+    {
+        curColor.a -= 0.01f;
+        gameObject.GetComponent<TextMeshProUGUI>().color = curColor;
+    }
+
+    void PlusAlpha()
+    {
+        curColor.a += 0.01f;
+        gameObject.GetComponent<TextMeshProUGUI>().color = curColor;
+    }
+}
+
+
