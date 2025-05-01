34660622b2979629e9183f56b4b9fd93662ce979
LeeJaeHo-96 <towpam123@gmail.com>
2025-05-01 17:21:35 +0900

    [Create] NPC 스크립터블 오브젝트 생성

---
+++ Assets/WorkSpace/LJH/Scripts/NPC/NpcSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcSpawner : MonoBehaviour
+{
+    private List<NPC> npcList = new List<NPC>();
+    void Start()
+    {
+        
+    }
+
+    
+}
