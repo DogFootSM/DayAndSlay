f5240938676abaf471eabdbf0c9920b9b0bfbffa
LeeJaeHo-96 <towpam123@gmail.com>
2025-05-02 16:31:38 +0900

    [Feat] 무기/방어구 제작대 UI 켜기끄기 구현

--- Assets/WorkSpace/LJH/Scripts/Forge/Forge.cs
+++ Assets/WorkSpace/LJH/Scripts/Forge/Forge.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Forge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject forgeUi;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        UiOnOffMethod(collision);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        
+        UiOnOffMethod(collision);
+    }
+
+    public virtual void UiOnOffMethod(Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            forgeUi.SetActive(!forgeUi.activeSelf);
+        }
     }
 }
