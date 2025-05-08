fe1b12ee84486dd92e5885576e00689a0c91a0dc
LeeJaeHo-96 <towpam123@gmail.com>
2025-05-08 16:03:49 +0900

    [Create] 재료 아이템 스크립터블 오브젝트 생성

---
+++ Assets/WorkSpace/LJH/Scriptable/Item/Ingredient/Ingredient.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Ingredient", menuName = "Scriptable Object/Ingredient")]
+public class Ingredient : ScriptableObject
+{
+    public string itemName;
+}
