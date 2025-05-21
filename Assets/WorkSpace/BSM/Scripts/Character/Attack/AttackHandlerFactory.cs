334c9c298d26b0f27bf7ad1bc9a82dfc289ff403
Vivid <suook15@naver.com>
2025-05-21 09:58:34 +0900

    [Chore] Handler 딕셔너리 객체 관리 추가

--- Assets/WorkSpace/BSM/Scripts/Character/Attack/AttackHandlerFactory.cs
+++ Assets/WorkSpace/BSM/Scripts/Character/Attack/AttackHandlerFactory.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public static class AttackHandlerFactory
 {
 
+    private static Dictionary<CharacterWeaponType, IAttackHandler> attackHandlerDict =
+        new Dictionary<CharacterWeaponType, IAttackHandler>();
+    
+    /// <summary>
+    /// 공격 핸들러 객체 반환
+    /// </summary>
+    /// <param name="weaponType">공격 타입 키</param>
+    /// <returns>핸들러 객체</returns>
     public static IAttackHandler ChangeAttackType(CharacterWeaponType weaponType)
     {
-        return weaponType switch
-        {
-            CharacterWeaponType.BOW => new RangeAttackHandler(),
-            CharacterWeaponType.WAND => new MeleeAttackHandler(),
-            CharacterWeaponType.SHORT_SWORD => new MeleeAttackHandler(),
-            CharacterWeaponType.LONG_SWORD => new MeleeAttackHandler(),
-            CharacterWeaponType.SPEAR => new MeleeAttackHandler(),
-            _ => null
-        };
+        if (!attackHandlerDict.ContainsKey(weaponType))
+        { 
+            attackHandlerDict[weaponType] = weaponType switch
+            {
+                CharacterWeaponType.BOW => new RangeAttackHandler(),
+                CharacterWeaponType.WAND => new MeleeAttackHandler(),
+                CharacterWeaponType.SHORT_SWORD => new MeleeAttackHandler(),
+                CharacterWeaponType.LONG_SWORD => new MeleeAttackHandler(),
+                CharacterWeaponType.SPEAR => new MeleeAttackHandler(),
+                _ => null
+            };
+        }
+
+        return attackHandlerDict[weaponType]; 
     }
     
 }
--- Assets/WorkSpace/BSM/Scripts/Character/Attack/MeleeAttackHandler.cs
+++ Assets/WorkSpace/BSM/Scripts/Character/Attack/MeleeAttackHandler.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeAttackHandler : IAttackHandler
-{
+{ 
+    
+    
     public void Attack()
     {
         Debug.Log("나는 근접 공격!");
