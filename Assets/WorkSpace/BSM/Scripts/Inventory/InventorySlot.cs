cde2fc3ba902ec804b9dc487c7c2c7724e0e3ffa
Vivid <suook15@naver.com>
2025-05-29 11:53:22 +0900

    [Feat] 인벤토리 아이템 추가

--- Assets/WorkSpace/BSM/Scripts/Inventory/InventorySlot.cs
+++ Assets/WorkSpace/BSM/Scripts/Inventory/InventorySlot.cs
@@ -1,10 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
-{ 
+{
+    public Item CurSlotItem => curSlotItem;
+    private Item curSlotItem;
     
+    private Image itemImage;
+
+    private bool isDrag;
     
+    private void Awake()
+    {
+        itemImage = transform.GetChild(1).GetComponent<Image>();
+
+        if (curSlotItem == null)
+        {
+            itemImage.color = Color.black;
+        }
+        
+    }
+
+    public void AddItem(Item item)
+    { 
+        curSlotItem = item;
+        itemImage.sprite = item.itemData.ItemImage;
+    }
+
+    public void ItemClicked()
+    {
+        isDrag = true;
+    }
     
 }
