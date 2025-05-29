9c88c05073f14df32f5e8c28bcbb0e2dfe11e22d
LeeJaeHo-96 <towpam123@gmail.com>
2025-05-29 12:00:34 +0900

    [Chore] 필요없는 using문 삭제

--- Assets/WorkSpace/LJH/Scripts/Dungeon/Monster/Animation/GeneralAnimator.cs
+++ Assets/WorkSpace/LJH/Scripts/Dungeon/Monster/Animation/GeneralAnimator.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
-using Zenject.SpaceFighter;
 
 public class GeneralAnimator : MonoBehaviour
 {
