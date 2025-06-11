52675b45e38465f6fe87dd4bd9b5acdc05832609
LeeJaeHo-96 <towpam123@gmail.com>
2025-06-11 16:36:08 +0900

    [Fix] 버튼래퍼 누락된 내용 복구

--- Assets/WorkSpace/LJH/Scripts/Common/ButtonWrapper.cs
+++ Assets/WorkSpace/LJH/Scripts/Common/ButtonWrapper.cs
@@ -6,35 +6,41 @@ using UnityEngine.UI;
 
 public class ButtonWrapper
 {
+    public string buttonName;
     public Button button;
     public WeaponType weaponType;
     public SubWeaponType subWeaponType;
     public Parts parts;
     public ItemData itemData;
 
-    public ButtonWrapper(Button button, WeaponType weaponType)
+    public ButtonWrapper(string buttonName, Button button, WeaponType weaponType)
     {
+        this.buttonName = buttonName;
         this.button = button;
         this.weaponType = weaponType;
     }
-    public ButtonWrapper(Button button, SubWeaponType subWeaponType)
+    public ButtonWrapper(string buttonName, Button button, SubWeaponType subWeaponType)
     {
+        this.buttonName = buttonName;
         this.button = button;
         this.subWeaponType = subWeaponType;
     }
-    public ButtonWrapper(Button button, Parts parts)
+    public ButtonWrapper(string buttonName, Button button, Parts parts)
     {
+        this.buttonName = buttonName;
         this.button = button;
         this.parts = parts;
     }
-    public ButtonWrapper(Button button, ItemData itemData)
+    public ButtonWrapper(string buttonName, Button button, ItemData itemData)
     {
+        this.buttonName = buttonName;
         this.button = button;
         itemData = button.GetComponent<ItemButton>().itemData;
         this.itemData = itemData;
     }
-    public ButtonWrapper(Button button)
+    public ButtonWrapper(string buttonName, Button button)
     {
+        this.buttonName = buttonName;
         this.button = button;
     }
 }
