47ff1ba2968c9717ef92ad7e6f4d0836f9682b8c
LeeJaeHo-96 <towpam123@gmail.com>
2025-07-09 19:08:25 +0900

    [Feat] 데이터베이스에 레시피 데이터베이스 추가

--- Assets/WorkSpace/LJH/Scripts/Manager/ItemDatabase.cs
+++ Assets/WorkSpace/LJH/Scripts/Manager/ItemDatabase.cs
@@ -8,11 +8,15 @@ using static UnityEditor.Progress;
 public class ItemDatabase : ScriptableObject
 {
     public List<ItemData> items;
+    public List<ItemRecipe> recipes;
     public List<int> ids;
 
     //최적화를 위해 Dictionary로 변환하여 관리
     [SerializeField][SerializedDictionary] SerializedDictionary<int, ItemData> itemIdToDataDict;
     [SerializeField][SerializedDictionary] SerializedDictionary<string, int> itemNameToIdDict;
+
+    [SerializeField][SerializedDictionary] SerializedDictionary<int, string> recipeIdToNameDict;
+    [SerializeField][SerializedDictionary] SerializedDictionary<string, ItemRecipe> recipeNameToDataDict;
 
     private void OnEnable()
     {
@@ -21,9 +25,15 @@ public class ItemDatabase : ScriptableObject
     private void InitDict()
     {
         itemIdToDataDict = new SerializedDictionary<int, ItemData>();
+        itemNameToIdDict = new SerializedDictionary<string, int>();
+
+        recipeIdToNameDict = new SerializedDictionary<int, string>();
+        recipeNameToDataDict = new SerializedDictionary<string, ItemRecipe>();
+
         foreach (ItemData item in items)
         {
             itemIdToDataDict[item.ItemId] = item;
+            recipeIdToNameDict[item.ItemId] = item.Name;
 
             if (ids.Count < items.Count)
             {
@@ -32,11 +42,17 @@ public class ItemDatabase : ScriptableObject
             }
         }
 
-        itemNameToIdDict = new SerializedDictionary<string, int>();
         foreach (int id in ids)
         {
             itemNameToIdDict[itemIdToDataDict[id].Name] = itemIdToDataDict[id].ItemId;
         }
+
+        foreach (ItemRecipe recipe in recipes)
+        {
+            recipeNameToDataDict[recipe.itemName] = recipe;
+        }
+
+        
     }
 
     public ItemData GetItemByID(int id)
