b697472aceb06f989b1ef054014b6c386fbf485a
LeeJaeHo-96 <towpam123@gmail.com>
2025-07-17 12:38:57 +0900

    [Chore] 팔레트 위치 변경

--- Assets/WorkSpace/LJH/Scripts/NPC/StateMachine/NpcDecisionState.cs
+++ Assets/WorkSpace/LJH/Scripts/NPC/StateMachine/NpcDecisionState.cs
@@ -14,18 +14,12 @@ public class NpcDecisionState : INpcState
         if (npc.IsBuyer)
         {
             //상점 문 위치로 이동
-            Debug.Log(npc);
-            Debug.Log(npc.GetComponentInChildren<TargetSensorNew>());
-            Debug.Log(npc.GetComponentInChildren<TargetSensorNew>().GetEnterPosition());
             Vector3 storeDoorPos = npc.GetComponentInChildren<TargetSensorNew>().GetEnterPosition();
             npc.StateMachine.ChangeState(new MoveState(npc, storeDoorPos));
         }
         else
         {
             // 랜덤 포지션으로 이동
-            Debug.Log(npc);
-            Debug.Log(npc.GetComponentInChildren<TargetSensorNew>());
-            Debug.Log(npc.GetComponentInChildren<TargetSensorNew>().GetRandomPosition());
             Vector3 randomPos = npc.GetComponentInChildren<TargetSensorNew>().GetRandomPosition();
             npc.StateMachine.ChangeState(new MoveState(npc, randomPos));
         }
