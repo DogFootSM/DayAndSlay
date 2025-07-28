8294705f23f5e04b0747e8bbff9fa1ba2ca4ba5f
LeeJaeHo-96 <towpam123@gmail.com>
2025-07-28 22:17:38 +0900

    [Feat] 커밋 누락된 보스몬스터 작업 커밋

--- Assets/WorkSpace/LJH/Scripts/Dungeon/Monster/Animation/BossAnimator.cs
+++ Assets/WorkSpace/LJH/Scripts/Dungeon/Monster/Animation/BossAnimator.cs
@@ -1,29 +1,33 @@
-using AYellowpaper.SerializedCollections;
 using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 
 [RequireComponent(typeof(Animator))]
 public class BossAnimator : MonoBehaviour
 {
-    protected Animator animator;
-    protected SpriteLibrary spriteLibrary;
+    private Animator animator;
+    private SpriteLibrary spriteLibrary;
 
-    [SerializedDictionary("ActionName", "SpriteLibrary")]
     [SerializeField]
-    protected SerializedDictionary<string, SpriteLibraryAsset> spriteDict = new SerializedDictionary<string, SpriteLibraryAsset>();
+    private SerializedDictionary<string, SpriteLibraryAsset> spriteDict =
+        new SerializedDictionary<string, SpriteLibraryAsset>();
 
     public BossMonsterStateMachine stateMachine;
 
     private int currentAttackHash;
-
     private bool isAction = false;
 
-    protected Dictionary<Direction, string> moveAction = new Dictionary<Direction, string>();
-    protected Dictionary<Direction, string> attackAction = new Dictionary<Direction, string>();
-    protected Dictionary<Direction, string> hitAction = new Dictionary<Direction, string>();
+    public bool IsPlayingAction
+    {
+        get { return isAction; }
+    }
 
-    Direction dir;
+    private Dictionary<Direction, string> moveAction = new Dictionary<Direction, string>();
+    private Dictionary<Direction, string> attackAction = new Dictionary<Direction, string>();
+    private Dictionary<Direction, string> hitAction = new Dictionary<Direction, string>();
+
+    private Direction dir;
 
     private void Start()
     {
@@ -31,88 +35,94 @@ public class BossAnimator : MonoBehaviour
         spriteLibrary = GetComponent<SpriteLibrary>();
 
         stateMachine = new BossMonsterStateMachine(this);
-
         stateMachine.ChangeState(new BossMonsterIdleState());
 
-        DictinaryInit();
+        InitDictionaries();
 
+        dir = Direction.Left;
     }
 
-    
     private void Update()
     {
-        dir = Direction.Left;
-
         if (isAction)
         {
-            //애니메이션 진행도 확인
             AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-            currentAttackHash = SetAttackHash(attackAction[dir]);
 
-            if (stateInfo.fullPathHash == currentAttackHash && stateInfo.normalizedTime >= 1f)
+            int attackHash = Animator.StringToHash("Base Layer." + attackAction[dir]);
+            int skillHash = Animator.StringToHash("Base Layer.BossSkillFirst");
+
+            if ((stateInfo.fullPathHash == attackHash || stateInfo.fullPathHash == skillHash) 
+                && stateInfo.normalizedTime >= 1f)
             {
                 isAction = false;
             }
         }
     }
-    
-    int SetAttackHash(string currentAttack)
-    {
-        return Animator.StringToHash("Base Layer." + currentAttack);
-    }
 
     public void PlayIdle()
     {
-		Debug.Log("아이들 재생");
+        if(isAction) return;
+        
         spriteLibrary.spriteLibraryAsset = spriteDict["Move"];
-        animator.Play("MonsterIdle");
+        animator.Play("BossIdle");
+        isAction = false;
     }
-    public void PlayMove() 
+
+    public void PlayMove()
     {
         if (isAction) return;
 
         spriteLibrary.spriteLibraryAsset = spriteDict["Move"];
-
         animator.Play(moveAction[dir]);
     }
-    public void PlayAttack() 
+
+    public void PlayAttack()
     {
         if (isAction) return;
 
         isAction = true;
         spriteLibrary.spriteLibraryAsset = spriteDict["Attack"];
-
         animator.Play(attackAction[dir]);
-
-    }
-    public void PlayHit() 
-    {
-        spriteLibrary.spriteLibraryAsset = spriteDict["Hit"];
-
-        animator.Play(hitAction[dir]);
-    }
-    public void PlayDie() 
-    {
-        spriteLibrary.spriteLibraryAsset = spriteDict["Die"];
-        animator.Play("MonsterDie");
     }
 
-    void DictinaryInit()
+    public void PlaySkill()
     {
-        moveAction.Add(Direction.Left, "MonsterMoveLeft");
-        moveAction.Add(Direction.Right, "MonsterMoveRight");
-        moveAction.Add(Direction.Up, "MonsterMoveUp");
-        moveAction.Add(Direction.Down, "MonsterMoveDown");
-
-        attackAction.Add(Direction.Left, "MonsterAttackLeft");
-        attackAction.Add(Direction.Right, "MonsterAttackRight");
-        attackAction.Add(Direction.Up, "MonsterAttackUp");
-        attackAction.Add(Direction.Down, "MonsterAttackDown");
-
-        hitAction.Add(Direction.Left, "MonsterHitLeft");
-        hitAction.Add(Direction.Right, "MonsterHitRight");
-        hitAction.Add(Direction.Up, "MonsterHitUp");
-        hitAction.Add(Direction.Down, "MonsterHitDown");
+        if (isAction) return;
+        
+        isAction = true;
+        spriteLibrary.spriteLibraryAsset = spriteDict["Attack"];
+        animator.Play("BossSkillFirst");
     }
     
+
+    public void PlayDie()
+    {
+        spriteLibrary.spriteLibraryAsset = spriteDict["Die"];
+        animator.Play("BossDie");
+        isAction = false;
+    }
+
+    public void PlayHit()
+    {
+        spriteLibrary.spriteLibraryAsset = spriteDict["Hit"];
+        animator.Play(hitAction[dir]);
+    }
+
+    private void InitDictionaries()
+    {
+        moveAction.Add(Direction.Left, "BossMoveLeft");
+        moveAction.Add(Direction.Right, "BossMoveRight");
+        moveAction.Add(Direction.Up, "BossMoveUp");
+        moveAction.Add(Direction.Down, "BossMoveDown");
+
+        attackAction.Add(Direction.Left, "BossAttackLeft");
+        attackAction.Add(Direction.Right, "BossAttackRight");
+        attackAction.Add(Direction.Up, "BossAttackUp");
+        attackAction.Add(Direction.Down, "BossAttackDown");
+
+        hitAction.Add(Direction.Left, "BossHitLeft");
+        hitAction.Add(Direction.Right, "BossHitRight");
+        hitAction.Add(Direction.Up, "BossHitUp");
+        hitAction.Add(Direction.Down, "BossHitDown");
+    }
 }
