b9c03bdff3a483894ea7cdecad457cd472ed4cc9
Vivid <suook15@naver.com>
2025-07-31 18:18:57 +0900

    [Feat] 초기 전체 레시피 아이템 리스트 생성

--- Assets/WorkSpace/BSM/Scripts/Recipe/RecipeBrowser.cs
+++ Assets/WorkSpace/BSM/Scripts/Recipe/RecipeBrowser.cs
@@ -8,9 +8,11 @@ public class RecipeBrowser : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown mainCategoryDropdown;
     [SerializeField] private TMP_Dropdown subCategoryDropdown;
+    [SerializeField] private GameObject recipeListParent;
+    [SerializeField] private GameObject recipeListElementPrefab;
     
     private ItemDatabaseManager itemDatabase => ItemDatabaseManager.instance;
-    private List<ItemRecipe> recipeList => itemDatabase.ItemDatabase.recipes;
+    private List<ItemData> recipeList => itemDatabase.ItemDatabase.items;
     
     private List<string> mainWeaponSubCategoryOptions = new List<string>()
     {
@@ -37,13 +39,28 @@ public class RecipeBrowser : MonoBehaviour
         "망토", "미정이1", "미정이2", "미정이3"
     };
     
-    private Dictionary<string, int> searchRecipeMap = new Dictionary<string, int>();
-
+    private Dictionary<Parts, List<ItemData>> searchRecipeMap = new Dictionary<Parts, List<ItemData>>();
+    
     private void Awake()
     {
         mainCategoryDropdown.onValueChanged.AddListener(x => ChangedSubCategoryOption(x));
+
+        for (int i = 0; i < recipeList.Count; i++)
+        {
+            if (!searchRecipeMap.ContainsKey(recipeList[i].Parts))
+            {
+                searchRecipeMap.Add(recipeList[i].Parts, new List<ItemData>());
+            }
+            
+            searchRecipeMap[recipeList[i].Parts].Add(recipeList[i]);
+            CreateRecipes(recipeList[i]);
+        }
     }
 
+    /// <summary>
+    /// 대분류 변경에 따른 중분류 카테고리 변경
+    /// </summary>
+    /// <param name="value"></param>
     private void ChangedSubCategoryOption(int value)
     {
         subCategoryDropdown.ClearOptions();
@@ -67,6 +84,17 @@ public class RecipeBrowser : MonoBehaviour
                 break;
         }
         subCategoryDropdown.RefreshShownValue();
+    }
+
+    /// <summary>
+    /// 초기 전체 아이템 레시피 리스트 생성
+    /// </summary>
+    /// <param name="recipeData"></param>
+    private void CreateRecipes(ItemData recipeData)
+    {
+        GameObject recipeListElement = Instantiate(recipeListElementPrefab, transform.position, Quaternion.identity, recipeListParent.transform);
+        RecipeElement recipeElement = recipeListElement.GetComponent<RecipeElement>();
+        recipeElement.SetElement(recipeData.ItemImage, recipeData.Name, recipeData.ItemDescA);
     }
     
 }
--- Assets/WorkSpace/BSM/Scripts/Recipe/RecipeElement.cs
+++ Assets/WorkSpace/BSM/Scripts/Recipe/RecipeElement.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RecipeElement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private TextMeshProUGUI itemDescription;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 레시피 요소 데이터 설정
+    /// </summary>
+    /// <param name="itemImange">해당 아이템의 이미지</param>
+    /// <param name="itemName">아이템의 이름</param>
+    /// <param name="itemDesc">아이템 설명</param>
+    public void SetElement(Sprite itemImange, string itemName, string itemDesc)
     {
-        
+        itemImage.sprite = itemImange;
+        this.itemName.text = itemName;
+        itemDescription.text = itemDesc;
     }
+    
 }
