d995d731ef39f39552e6940d186cc41a5348ee9c
Vivid <suook15@naver.com>
2025-07-31 16:38:20 +0900

    [Chore] 중간 머지 커밋

--- Assets/WorkSpace/BSM/Scripts/Recipe/RecipeBrowser.cs
+++ Assets/WorkSpace/BSM/Scripts/Recipe/RecipeBrowser.cs
@@ -6,14 +6,37 @@ using UnityEngine;
 
 public class RecipeBrowser : MonoBehaviour
 {
-    private ItemDatabaseManager itemDatabase => ItemDatabaseManager.instance;
+    [SerializeField] private TMP_Dropdown mainCategoryDropdown;
+    [SerializeField] private TMP_Dropdown subCategoryDropdown;
     
-    [SerializeField]
+    private ItemDatabaseManager itemDatabase => ItemDatabaseManager.instance;
     private List<ItemRecipe> recipeList => itemDatabase.ItemDatabase.recipes;
+
+    private List<string> mainWeaponSubCategoryOptions = new List<string>()
+    {
+        "검", "창", "활", "완드"
+    };
+
+    private List<string> subWeaponSubCategoryOptions = new List<string>()
+    {
+        "미정이", "엠블렘", "화살통", "마도서" 
+    };
+
+    private List<string> armorSubCategoryOptions = new List<string>()
+    {
+        "모자", "상의", "하의", "장갑", "신발"
+    };
+
+    private List<string> accessorySubCategoryOptions = new List<string>()
+    {
+        "망토", "미정이1", "미정이2", "미정이3"
+    };
     
     private Dictionary<string, int> searchRecipeMap = new Dictionary<string, int>();
-    private TMP_Dropdown test;
+
     private void Awake()
     { 
+        
+        
     }
 }
