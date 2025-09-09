43b39b483024da03a17be10eae9605d68c8af70c
Vivid <suook15@naver.com>
2025-09-10 03:12:35 +0900

    [Etc] 스크립트 위치 변경 및 활 스킬 6번 이펙트 추가
    1. 활 5, 6번 스킬 폴더 위치 변경
    2. 활 6번 이펙트 추가

---
+++ Assets/WorkSpace/BSM/Scripts/Skill/MeleeSkills/BOW/BOAS006.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOAS006 : MeleeSkill
+{
+    public BOAS006(SkillNode skillNode) : base(skillNode)
+    {
+    }
+
+    public override void UseSkill(Vector2 direction, Vector2 playerPosition)
+    {
+        
+        
+    }
+
+    public override void ApplyPassiveEffects(CharacterWeaponType weaponType)
+    {
+    }
+
+    public override void Gizmos()
+    {
+    }
+}
