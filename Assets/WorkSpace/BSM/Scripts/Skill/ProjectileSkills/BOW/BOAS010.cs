885a10276d8df7b55ae1da0a4574cba5f10bb87e
Vivid <suook15@naver.com>
2025-09-15 10:06:31 +0900

    [Chore] 중간 머지 커밋

--- Assets/WorkSpace/BSM/Scripts/Skill/ProjectileSkills/BOW/BOAS010.cs
+++ Assets/WorkSpace/BSM/Scripts/Skill/ProjectileSkills/BOW/BOAS010.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BOAS010 : MeleeSkill
 {
@@ -21,14 +22,51 @@ public class BOAS010 : MeleeSkill
         pos = playerPosition;
         dir = direction;
 
-        Collider2D[] cols = Physics2D.OverlapBoxAll(playerPosition + (direction * skillNode.skillData.SkillRange), overlapSize, 0, monsterLayer);
+        Collider2D[] cols = Physics2D.OverlapBoxAll(playerPosition + (direction * 4), overlapSize, 0, monsterLayer);
         
-        
-        
-        
-        
+        skillNode.PlayerSkillReceiver.StartCoroutine(SkillEffectRoutine(playerPosition, direction, cols)); 
     }
 
+    private IEnumerator SkillEffectRoutine(Vector2 playerPosition, Vector2 direction, Collider2D[] cols)
+    {
+        float elapsedTime = 0;
+        
+        float minX = -overlapSize.x / 2;
+        float maxX = overlapSize.x / 2;
+        float maxY = overlapSize.y / 2;
+        
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime;
+            
+            float x = Random.Range(minX, maxX);
+            float y = Random.Range(0, maxY);
+            
+            GameObject instance = particlePooling.GetSkillPool($"{skillNode.skillData.SkillId}_1_Particle",
+                skillNode.skillData.SkillEffectPrefab[0]);
+            
+            instance.SetActive(true);
+            instance.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(235, 255));
+            instance.transform.position = playerPosition + (direction * 4) + new Vector2(x, y);
+            
+            ParticleSystem particleSystem = instance.GetComponent<ParticleSystem>();
+            particleSystem.Play();
+            
+            ParticleInteraction interaction = instance.GetComponent<ParticleInteraction>();
+            interaction.EffectId = $"{skillNode.skillData.SkillId}_1_Particle";
+            interaction.LinearProjectile(0, instance.transform.right, 2);
+ 
+            yield return WaitCache.GetWait(0.01f);
+        }
+
+        for (int i = 0; i < cols.Length; i++)
+        {
+            IEffectReceiver receiver = cols[i].GetComponent<IEffectReceiver>();
+            Hit(receiver, skillDamage, skillNode.skillData.SkillHitCount);
+        }
+        
+    }
+    
     public override void ApplyPassiveEffects(CharacterWeaponType weaponType)
     {
     }
@@ -36,6 +74,6 @@ public class BOAS010 : MeleeSkill
     public override void Gizmos()
     {
         UnityEngine.Gizmos.color = Color.yellow;
-        UnityEngine.Gizmos.DrawWireCube(pos + (dir * skillNode.skillData.SkillRange), overlapSize);
+        UnityEngine.Gizmos.DrawWireCube(pos + (dir * 4), overlapSize);
     }
 }
