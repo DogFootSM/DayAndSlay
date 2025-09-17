fb03e0483318ea08ceb189740dc996703b4a4c94
Vivid <suook15@naver.com>
2025-09-17 10:39:43 +0900

    [Refactor] 완드 5번 스킬 수정

--- Assets/WorkSpace/BSM/Scripts/Skill/MeleeSkills/WAND/WDAS005.cs
+++ Assets/WorkSpace/BSM/Scripts/Skill/MeleeSkills/WAND/WDAS005.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class WDAS005 : MeleeSkill
 {
-    private int shieldCount = 2;
-    private int skillDuration = 30;
+    private int shieldCount = 2; 
+
+    private Action action;
+    private Coroutine castingCo;
+    
     public WDAS005(SkillNode skillNode) : base(skillNode)
     {
     }
 
     public override void UseSkill(Vector2 direction, Vector2 playerPosition)
     {
-        ExecuteCasting(skillNode.skillData.SkillCastingTime);
-        
         ListClear();
-        SkillEffect(playerPosition, 0, $"{skillNode.skillData.SkillId}_1_Particle", skillNode.skillData.SkillEffectPrefab[0]);
-        ExecuteShield(shieldCount, skillNode.skillData.SkillAbilityValue, skillDuration);
+        ExecuteCasting(skillNode.skillData.SkillCastingTime);
+
+        action = ExecutePostCastAction;
+        castingCo = skillNode.PlayerSkillReceiver.StartCoroutine(WaitCastingRoutine(action)); 
     }
 
+    private void ExecutePostCastAction()
+    {
+        ExecuteFollowCharacterWithParticle(skillNode.skillData.SkillEffectPrefab[0], skillNode.skillData.BuffDuration,$"{skillNode.skillData.SkillId}_1_Particle");
+        ExecuteShield(shieldCount, skillNode.skillData.SkillAbilityValue, skillNode.skillData.BuffDuration);
+    }
+    
+    
     public override void ApplyPassiveEffects(CharacterWeaponType weaponType)
     {
     }
