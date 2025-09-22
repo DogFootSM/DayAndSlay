ceb96744f86c074a143787d4b039a51bddd15591
Vivid <suook15@naver.com>
2025-09-22 13:48:15 +0900

    [Refactor] 매직 미사일 몬스터 타격 로직 수정

--- Assets/WorkSpace/BSM/Scripts/Skill/ProjectileSkills/WAND/WDAS001.cs
+++ Assets/WorkSpace/BSM/Scripts/Skill/ProjectileSkills/WAND/WDAS001.cs
@@ -32,12 +32,12 @@ public class WDAS001 : ProjectileSkill
     /// <returns></returns>
     private void ExecutePostCastAction(Vector2 position, Vector2 direction)
     {
-        skillNode.skillData.SkillEffectPrefab[1].GetComponent<MagicMissile>().SetData(skillNode.skillData.SkillHitCount, skillNode.skillData.SkillDamage);
         SingleEffect(position + direction, skillNode.skillData.SkillEffectPrefab[0], $"{skillNode.skillData.SkillId}_1_Particle", effectIndex);
         
         //몸 주변에 발사 이펙트 재생
         SingleEffect(position + direction, skillNode.skillData.SkillEffectPrefab[1], $"{skillNode.skillData.SkillId}_2_Particle", ++effectIndex);
-        
+        surroundEffectInstance.GetComponent<MagicMissile>().SetData(skillNode.skillData.DetectedCount, skillNode.skillData.SkillHitCount, skillNode.skillData.SkillDamage);
+
         //히트 이펙트 풀에 반납할 ID 설정
         surroundInteraction[effectIndex].SetHitEffectId($"{skillNode.skillData.SkillId}_3_Particle");
         
