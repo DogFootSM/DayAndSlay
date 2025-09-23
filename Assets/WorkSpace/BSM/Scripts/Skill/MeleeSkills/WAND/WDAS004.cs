8b9d888c2e954fa6b7141183d44ccd646b6e1eca
Vivid <suook15@naver.com>
2025-09-23 11:51:48 +0900

    [Chore] 완드 4번 <-> 10번 스킬 변경

--- Assets/WorkSpace/BSM/Scripts/Skill/MeleeSkills/WAND/WDAS004.cs
+++ Assets/WorkSpace/BSM/Scripts/Skill/MeleeSkills/WAND/WDAS004.cs
@@ -10,9 +10,6 @@ public class WDAS004 : MeleeSkill
     private Vector2 hitPos;
     private Coroutine castingCo;
     
-    
-    private int thunderCount = 2;
-    
     public WDAS004(SkillNode skillNode) : base(skillNode)
     {
     } 
@@ -37,6 +34,7 @@ public class WDAS004 : MeleeSkill
     {
         Collider2D[] cols = Physics2D.OverlapBoxAll(hitPos, overlapSize, 0, monsterLayer);
         Sort.SortMonstersByNearest(cols, playerPosition);
+        
         //감지 몬스터가 없을 경우 바라보는 방향에 낙뢰 위치 표시 재생 후 return
         if (cols.Length < 1)
         {
@@ -46,46 +44,24 @@ public class WDAS004 : MeleeSkill
         
         int detected = skillNode.skillData.DetectedCount < cols.Length ? skillNode.skillData.DetectedCount : cols.Length;
         
-        for (int i = 0; i < detected; i++)
+        for (int j = 0; j < detected; j++)
         {
-            //몬스터 하단 번개 위치 이펙트
-            SkillEffect(cols[i].transform.position + new Vector3(0, -0.5f), i,$"{skillNode.skillData.SkillId}_1_Particle" ,skillNode.skillData.SkillEffectPrefab[0]);
+            SkillEffect(cols[j].transform.position, j, $"{skillNode.skillData.SkillId}_2_Particle", skillNode.skillData.SkillEffectPrefab[0]);
+                
+            skillActions.Add(new List<Action>());
+                
+            IEffectReceiver receiver = cols[j].GetComponent<IEffectReceiver>();
+                
+            skillActions[j].Add(() => Hit(receiver, skillDamage, skillNode.skillData.SkillHitCount));
+            skillActions[j].Add(() => ExecuteStun(receiver, skillNode.skillData.DeBuffDuration));
+            skillActions[j].Add(() => RemoveTriggerModuleList(0));
+                
+            triggerModules[j].AddCollider(cols[j]);
+            interactions[j].ReceiveAction(skillActions[j]);
         }
-
-        skillNode.PlayerSkillReceiver.StartCoroutine(ThunderEffectRoutine(cols));
+        
     }
 
-    /// <summary>
-    /// 번개 이펙트 코루틴
-    /// </summary> 
-    /// <returns></returns>
-    private IEnumerator ThunderEffectRoutine(Collider2D[] cols)
-    { 
-        for (int i = 0; i < thunderCount; i++)
-        {
-            ListClear();
-            
-            for (int j = 0; j < cols.Length; j++)
-            {
-                SkillEffect(cols[j].transform.position, j, $"{skillNode.skillData.SkillId}_2_Particle", skillNode.skillData.SkillEffectPrefab[1]);
-                
-                skillActions.Add(new List<Action>());
-                
-                IEffectReceiver receiver = cols[j].GetComponent<IEffectReceiver>();
-                
-                skillActions[j].Add(() => Hit(receiver, skillDamage, skillNode.skillData.SkillHitCount));
-                skillActions[j].Add(() => ExecuteStun(receiver, skillNode.skillData.DeBuffDuration));
-                skillActions[j].Add(() => RemoveTriggerModuleList(0));
-                
-                triggerModules[j].AddCollider(cols[j]);
-                interactions[j].ReceiveAction(skillActions[j]);
-            }
-
-            yield return WaitCache.GetWait(0.6f);
-        } 
-    }
-    
-     
     public override void ApplyPassiveEffects(CharacterWeaponType weaponType)
     {
     }
