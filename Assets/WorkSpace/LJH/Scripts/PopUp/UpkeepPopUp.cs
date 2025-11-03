b5465a3b46033b3209fa9b1c46f24cca4e06f05c
LeeJaeHo-96 <towpam123@gmail.com>
2025-11-03 21:51:33 +0900

    [Feat] 유지비 시스템 및 UI

---
+++ Assets/WorkSpace/LJH/Scripts/PopUp/UpkeepPopUp.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UpkeepPopUp : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField]private List<GameObject> Upkeeptexts;
+    
+    [SerializeField] private float animationDuration = 0.75f; 
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayOpen()
+    {
+        animator.Play("UpkeepOpen");
+        StartCoroutine(OpenAnimationCompleteCoroutine());
+    }
+
+    public void PlayClose()
+    {
+        animator.Play("UpkeepClose");
+        StartCoroutine(CloseAnimationCompleteCoroutine());
+    }
+    
+    
+    private IEnumerator OpenAnimationCompleteCoroutine()
+    {
+        yield return new WaitForSeconds(animationDuration);
+
+        AllTextOn();
+    }
+
+    private IEnumerator CloseAnimationCompleteCoroutine()
+    {
+        AllTextOff();
+        
+        yield return new WaitForSeconds(animationDuration);
+
+        gameObject.SetActive(false); 
+    }
+
+    private void AllTextOn()
+    {
+        foreach (GameObject text in Upkeeptexts)
+        {
+            text.SetActive(true);
+        }
+    }
+
+    private void AllTextOff()
+    {
+        foreach (GameObject text in Upkeeptexts)
+        {
+            text.SetActive(false);
+        }
+    }
+}
