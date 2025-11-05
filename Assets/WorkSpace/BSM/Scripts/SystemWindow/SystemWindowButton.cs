034e7d593b865b45ea38d5c71107c078472485e4
Vivid <suook15@naver.com>
2025-11-06 03:34:04 +0900

    [Chore] Book Prefab 생성
    1. 원본 Book Prefab 생성
    2. Book Variant Prefab 생성

--- Assets/WorkSpace/BSM/Scripts/SystemWindow/SystemWindowButton.cs
+++ Assets/WorkSpace/BSM/Scripts/SystemWindow/SystemWindowButton.cs
@@ -8,17 +8,27 @@ public class SystemWindowButton : MonoBehaviour
 {
     [SerializeField] private SystemType systemType;
     [SerializeField] private SystemWindowController windowController;
-
+    
     private Button systemWindowButton;
+    private Animator _tabAnimator;
+    
+    private int _tabOpenAnimHash = Animator.StringToHash("TabOpen");
+    private int _tabCloseAnimHash = Animator.StringToHash("TabClose");
     
     private void Awake() => Init();
 
     private void Init()
     {
         systemWindowButton = GetComponent<Button>();
+        _tabAnimator = GetComponent<Animator>();
         
         systemWindowButton.onClick.AddListener(() => windowController.OpenSystemWindow(systemType));
     }
-     
-    
+
+    public void SwitchTab(bool isOpen)
+    {
+        int playAnim = isOpen ? _tabOpenAnimHash : _tabCloseAnimHash;
+        
+        _tabAnimator.Play(playAnim);
+    } 
 }
--- Assets/WorkSpace/BSM/Scripts/SystemWindow/SystemWindowController.cs
+++ Assets/WorkSpace/BSM/Scripts/SystemWindow/SystemWindowController.cs
@@ -12,12 +12,24 @@ public class SystemWindowController : MonoBehaviour
     [SerializeField] private GameObject parentCanvas;
     [SerializeField] private GameObject menuCanvas;
     [SerializeField] private GameObject quickSlotCanvas;
+    [Header("키 입력 가능 상태 여부")]
+    [SerializeField] private bool canInputKey;
+    [SerializeField] private Animator _bookAnimator;
+    
+    [SerializedDictionary("SystemType", "SystemWindowTabButton")] [SerializeField] 
+    private SerializedDictionary<SystemType, SystemWindowButton> systemWindowButtons;
     
     private Stack<GameObject> canvasStack = new Stack<GameObject>();
     private SystemType currentSystemType = SystemType.SIZE;
     
+    private int _bookFlippingAnimHash = Animator.StringToHash("BookFlipping");
+    private int _bookFlippingReverseAnimHash = Animator.StringToHash("BookFlippingReverse");
+    private int _bookResetTriggerHash = Animator.StringToHash("BookReset");
+    
     private void Update()
     {
+        if (!canInputKey) return;
+        
         InputSystemKey();
     }
 
@@ -28,6 +40,26 @@ public class SystemWindowController : MonoBehaviour
     public void OpenSystemWindow(SystemType systemType)
     {
         GameObject openWindow = systemWindows[systemType];
+        
+        //클릭한 신규 탭 애니메이션 전환
+        systemWindowButtons[systemType].SwitchTab(true);
+
+        //시스템 창 최초 오픈 상태가 아닌 상태에서 탭 변경 시 기존 탭 애니메이션 전환
+        if (currentSystemType != SystemType.SIZE)
+        {
+            systemWindowButtons[currentSystemType].SwitchTab(false);
+
+            if ((int)systemType > (int)currentSystemType)
+            {
+                _bookAnimator.Play(_bookFlippingAnimHash);
+            }
+            else
+            {
+                _bookAnimator.Play(_bookFlippingReverseAnimHash);
+            } 
+        }
+        
+        //현재 오픈된 시스템 타입 할당
         currentSystemType = systemType;
         
         if (canvasStack.Count == 0)
@@ -54,8 +86,8 @@ public class SystemWindowController : MonoBehaviour
         while (canvasStack.Count != 0)
         {
             canvasStack.Pop().SetActive(false);
-
-            if (quickSlotCanvas.activeSelf)
+    
+            if (quickSlotCanvas != null && quickSlotCanvas.activeSelf)
             {
                 quickSlotCanvas.SetActive(false);
             } 
@@ -72,15 +104,39 @@ public class SystemWindowController : MonoBehaviour
         {
             canvasStack.Pop().SetActive(false);
             
-            if (quickSlotCanvas.activeSelf)
+            if (quickSlotCanvas != null && quickSlotCanvas.activeSelf)
             {
                 quickSlotCanvas.SetActive(false);
             }
         }
         
         canvasStack.Push(switchWindow);
-        switchWindow.SetActive(true);
+
+        StartCoroutine(CheckFlipingAnimationRoutine(switchWindow));
     }
+
+
+    private IEnumerator CheckFlipingAnimationRoutine(GameObject switchWindow)
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (_bookAnimator.GetCurrentAnimatorStateInfo(0).IsName("BookFlipping") ||
+                _bookAnimator.GetCurrentAnimatorStateInfo(0).IsName("BookFlippingReverse"))
+            {
+                float animTime = _bookAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
+                if (animTime >= 1.0f)
+                {
+                    switchWindow.SetActive(true);
+                    _bookAnimator.SetTrigger(_bookResetTriggerHash);
+                    break;
+                } 
+            } 
+        }
+    }
+    
     
     /// <summary>
     /// 시스템 키 입력 감지
