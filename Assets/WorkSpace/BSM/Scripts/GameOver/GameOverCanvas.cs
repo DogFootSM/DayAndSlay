2757149bbc063bdea531c1caae33e5a370279a18
Vivid <suook15@naver.com>
2025-11-28 11:55:35 +0900

    [Chore] 캐릭터 프리팹 구조 수정

--- Assets/WorkSpace/BSM/Scripts/GameOver/GameOverCanvas.cs
+++ Assets/WorkSpace/BSM/Scripts/GameOver/GameOverCanvas.cs
@@ -8,26 +8,28 @@ using Zenject;
 
 public class GameOverCanvas : MonoBehaviour
 {
-    [Inject(Id = "outSideScene")]
-    private SceneReference townScene;
     
-    [Inject(Id = "LoadingScene")]
-    private SceneReference loadingScene;
+    [SerializeField] private SceneReference townScene;
+    [SerializeField] private SceneReference loadingScene;
     
     [SerializeField] private Button confirmButton;
     [SerializeField] private PlayerRoot playerRoot;
     [SerializeField] private PlayerController playerController;
+
+    [Inject] private MapManager mapManager;
     
     private void Awake()
     {
         confirmButton.onClick.AddListener(MoveToTown);
+        ProjectContext.Instance.Container.Inject(this);
     }
 
     private void MoveToTown()
     {
         Debug.Log($"마을로 이동 {playerRoot == null}");
         playerController.PlayerResurrection();
-        playerRoot.TranslateScenePosition(new Vector2(0, 0));
+        playerRoot.TranslateScenePosition(new Vector2(-47.22f, -16f));
+        mapManager.MapChange(MapType.TOWN_OUTSIDE);
         Loading.LoadScene(townScene);
         SceneManager.LoadScene(loadingScene.Name);
     } 
