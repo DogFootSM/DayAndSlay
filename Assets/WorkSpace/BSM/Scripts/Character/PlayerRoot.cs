0d1ccd2ef4e37dfb84a0a200578f24604041688a
Vivid <suook15@naver.com>
2025-12-19 11:53:36 +0900

    [Feat] 캐릭터 생성 시 저장 시점 불러오기 추가
    1. 불러온 CurrentTime이 1일 경우 밤, 0일 경우 낮 전환

--- Assets/WorkSpace/BSM/Scripts/Character/PlayerRoot.cs
+++ Assets/WorkSpace/BSM/Scripts/Character/PlayerRoot.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using Zenject;
 
 public class PlayerRoot : MonoBehaviour
 {
     public static PlayerRoot PlayerRootInstance;
 
+    [Inject] private DataManager dataManager;
+    [Inject] private SqlManager sqlManager;
+    
+    private DayManager dayManager => DayManager.instance;
+    private int currentTimeState = 0;
+    
     private void Awake()
     {
         if (PlayerRootInstance == null)
@@ -18,10 +26,42 @@ public class PlayerRoot : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        
+        ProjectContext.Instance.Container.Inject(this);
+        SetDayOrNightState(); 
     }
-    
+  
     public void TranslateScenePosition(Vector2 position)
     {
         transform.position = position;
     }
+
+    /// <summary>
+    /// DB에서 저장 시점을 불러옴
+    /// 저장 시점에 따라 DayManager Morning or Night 설정
+    /// </summary>
+    private void SetDayOrNightState()
+    {
+        IDataReader reader = sqlManager.ReadDataColumn(
+            new[] { sqlManager.GetCharacterColumn(CharacterDataColumns.CURRENTTIME) },
+            new[] { sqlManager.GetCharacterColumn(CharacterDataColumns.SLOT_ID) }, new[] { $"{dataManager.SlotId}" },
+            null);
+        
+        while (reader.Read())
+        {
+            currentTimeState = reader.GetInt32(0);
+        }
+
+        switch (currentTimeState)
+        {
+            case 0:
+                dayManager.StartMorning();
+                break;
+            
+            case 1:
+                dayManager.StartNight();
+                break;
+        } 
+    }
+    
 }
